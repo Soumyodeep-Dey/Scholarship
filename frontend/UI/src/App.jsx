ea83eaaa5fea197cb070831d23895532ae7fc6c3
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2024-11-04 08:46:38 +0530

    try and error

--- frontend/UI/src/App.jsx
+++ frontend/UI/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1>hii</h1>
     </>
   )
 }
 
-export default App
+export default App;
